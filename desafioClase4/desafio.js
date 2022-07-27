ed6a3c429706ca91e9c41ddb839def77a54c4e51
Bruno Niello <brunoniello@gmail.com>
2022-07-27 04:21:57 -0300

    casi terminado

--- desafioClase4/desafio.js
+++ desafioClase4/desafio.js
@@ -7,66 +7,33 @@ class Contenedor {
     //guardo un objeto nuevo y escribo el archivo de no existir
     async save(obj){
         try {
-            const productos = await this.getAll();
-            if (!productos) {
-                
-            } else {
 
+            const productos = await this.getAll();
+
+            let id = 0;
+            let array = [];
+            let objeto;
+
+            if(productos) {
+                productos.filter(x=> x.name )
+                id = 1 + parseInt(productos.length);
+                const newObjeto = {...obj, id: id};
+                array.push(...productos, newObjeto);  
+                objeto = JSON.stringify(array, null, 2);
+            } else { 
+                id = 1;
+                const newObjeto = {...obj, id: id};
+                array.push(newObjeto); 
+                objeto = JSON.stringify(array, null, 2);
             }
 
-            // let array = [];
-            // let id = Math.floor(Math.random() * 999);
-            // const newObjeto = {...obj, id: id}
-            // array.push(newObjeto);  
-            // const productos = await this.getAll();
-            // const objeto = JSON.stringify(array, null, 2);
-
-            // await fs.promises.writeFile(this.archivo, objeto, (error)=>{
-            //     if(error) {
-            //         throw new Error('error de escritura')
-            //     }
-            //     console.log('escritura exitosa')
-            //     })
-            // return (objeto.id)
-            
-            // if(productos == ""){
-            //     await fs.promises.writeFile(this.archivo, `[${objeto},]`, (error)=>{
-            //         if(error) {
-            //             throw new Error('error de escritura')
-            //         }
-            //         console.log('escritura exitosa')
-            //     })
-            //     return (objeto.id)
-            // }else{
-
-            //     let newArray = productos.map()
-
-
-
-
-
-
-
-
-            //     // appendFile no me sirvio porque no guarda el array, sino los objetos
-            //     let string = JSON.stringify(productos);
-            //     // await this.deleteAll();
-            //     await fs.promises.appendFile(this.archivo, `${objeto},`);
-            //     // productos.push(newObjeto)
-            //     // let string = JSON.stringify(productos);
-            //     // console.log(productos)
-
-
-            //     // await fs.promises.unlink(this.archivo);
-            //     // await fs.promises.writeFile(this.archivo, string, (error)=>{
-            //     //     if(error) {
-            //     //         throw new Error('error de escritura')
-            //     //     }
-            //     //     console.log('escritura exitosa')
-            //     // })
-            //     return (objeto.id)
-            // }
-
+            await fs.promises.writeFile(this.archivo, objeto, (error)=>{
+                if(error) {
+                    throw new Error('error de escritura')
+                }
+                console.log('escritura exitosa')
+                })
+            return (objeto.id)
 
         } catch (error) {
             console.error(error);
@@ -135,25 +102,6 @@ const prueba = async () => {
 
     const productos = new Contenedor('./productos.txt');
 
-    //defino 3 objetos de prueba, los defino
-
-    // let productosNuevos = [
-    //     {
-    //     title: 'pipa butz choquin',
-    //     price: '10000',
-    //     thumbnail: 'https://firebasestorage.googleapis.com/v0/b/mefe-pipas.appspot.com/o/pipa-bc01.jpg?alt=media&token=6e328d93-41f8-4350-b503-c3072c43744f'
-    //     },
-    //     {
-    //     title: 'pipa peterson',
-    //     price: '15000',
-    //     thumbnail: 'https://firebasestorage.googleapis.com/v0/b/mefe-pipas.appspot.com/o/pipa-pe01.jpg?alt=media&token=3a1addb3-192a-46c8-94b8-2a1a0560c3b9' 
-    //     },
-    //     {
-    //     title: 'pipa dunhill',
-    //     price: '20000',
-    //     thumbnail: 'https://firebasestorage.googleapis.com/v0/b/mefe-pipas.appspot.com/o/pipa-pe01.jpg?alt=media&token=3a1addb3-192a-46c8-94b8-2a1a0560c3b9' 
-    //     }
-    // ];
 
     let productosNuevos1 = {
             title: 'pipa dunhill',
@@ -166,9 +114,16 @@ const prueba = async () => {
             price: '15000',
             thumbnail: 'https://firebasestorage.googleapis.com/v0/b/mefe-pipas.appspot.com/o/pipa-pe01.jpg?alt=media&token=3a1addb3-192a-46c8-94b8-2a1a0560c3b9' 
             }
+    let productosNuevos3 = {
+        title: 'pipa mefe',
+        price: '20000',
+        thumbnail: 'https://firebasestorage.googleapis.com/v0/b/mefe-pipas.appspot.com/o/pipa-pe01.jpg?alt=media&token=3a1addb3-192a-46c8-94b8-2a1a0560c3b9' 
+        }
+
     //escribo en el archivo los tres productos // ejecutar solo una vez, o cuando se quiera guardar algo
     await productos.save(productosNuevos1);
     await productos.save(productosNuevos2);
+    await productos.save(productosNuevos3);
     
 
     //traigo todos los productos a la consola
