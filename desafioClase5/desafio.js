351115a086f866426a3e58d7a5c8d8580ee25578
Bruno Niello <brunoniello@gmail.com>
2022-07-27 21:23:55 -0300

    clase5

--- desafioClase5/desafio.js
+++ desafioClase5/desafio.js
@@ -1,5 +1,25 @@
 const http = require('http');
 
-const server = http.createServer((peticion, respuesta)=>{
+const getMensaje = () => {
     
+    const horario = new Date().getHours();
+    let mensaje; 
+    
+    if(horario >= 6 && horario <= 12){
+        mensaje = "buenos dias!"
+    } else if (horario >= 13 && horario <= 19) {
+        mensaje = "buenas tardes!"
+    } else {
+        mensaje = "buenas noches!"
+    }
+
+    return mensaje;
+}
+
+const server = http.createServer((peticion, respuesta)=>{
+    respuesta.end(getMensaje());
+})
+
+const connectedServer = server.listen(8080, ()=>{
+    console.log(`Server http escuchando en el puerto ${connectedServer.address().port}`)
 })
