2b34947cf95be19929c96b35a9d9833d4d13e84a
Bruno Niello <brunoniello@gmail.com>
2022-08-01 13:06:17 -0300

    desafio clase 6 terminado

---
+++ desafioClase6/contenedor.js
@@ -0,0 +1,163 @@
+const fs = require('fs'); 
+
+// const { promises: fs } = require('fs');
+
+class Contenedor {
+    constructor(archivo){
+        this.archivo = archivo;
+    }
+
+    //guardo un objeto nuevo y escribo el archivo de no existir
+    async save(obj){
+
+        let id = 0;
+        let objeto;
+        let array = [];
+
+        try {
+
+            const productos = await this.getAll();
+
+            if(productos){
+
+                const arrayFiltrado = productos.map(item => item.title);
+                const comprobacion = arrayFiltrado.includes(obj.title);
+
+                if(comprobacion === false) {
+                
+                    id = 1 + parseInt(productos.length);
+                    const newObjeto = {...obj, id: id};
+                    array.push(...productos, newObjeto);  
+                    objeto = JSON.stringify(array, null, 2);
+
+                    await fs.promises.writeFile(this.archivo, objeto, (error)=>{
+                        if(error) {
+                            throw new Error('error de escritura')
+                        }
+                        console.log('escritura exitosa')
+                        })
+                    return (objeto.id)
+                }
+            } else { 
+                id = 1;
+                const newObjeto = {...obj, id: id};
+                array.push(newObjeto); 
+                objeto = JSON.stringify(array, null, 2);
+
+                await fs.promises.writeFile(this.archivo, objeto, (error)=>{
+                    if(error) {
+                        throw new Error('error de escritura')
+                    }
+                    console.log('escritura exitosa')
+                    })
+                return (objeto.id)
+            }
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    //obtengo un objeto por id
+    async getById(id){
+        try {
+            const productos = await this.getAll();
+            const productoId = productos.find(x => x.id == id);
+            return console.log(productoId);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    //obtengo todos los objetos del archivo
+    async getAll(){
+        try {
+            const productos = await fs.promises.readFile(this.archivo, 'utf-8');
+            const productosArray = JSON.parse(productos);
+            return productosArray
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    //elimino un objeto por id
+    async deleteById(id){
+        try {
+            const productos = await this.getAll();
+            const deleteId = productos.filter(x => x.id !== id);
+            const productosFiltrados = JSON.stringify(deleteId, null, 2);
+            await fs.promises.writeFile(this.archivo, productosFiltrados, (error)=>{
+                if(error) {
+                    throw new Error('error de borrado')
+                }
+                console.log('borrado exitoso')
+            })
+
+        } catch (error) {
+            console.error(error)
+        }
+        
+
+    }
+    //borro todos los productos del archivo
+    async deleteAll(){
+        try {
+            await fs.promises.unlink(this.archivo);
+            await fs.promises.writeFile(this.archivo, "", (error)=>{
+                if(error) {
+                    throw new Error('error de borrado')
+                }
+                console.log('borrado exitoso')
+            })
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+}
+
+
+
+const live = async () => {
+
+    const productos = new Contenedor('./productos.txt');
+
+
+    const productosNuevos1 = {
+            title: 'pipa dunhill',
+            price: '20000',
+            thumbnail: 'https://firebasestorage.googleapis.com/v0/b/mefe-pipas.appspot.com/o/pipa-pe01.jpg?alt=media&token=3a1addb3-192a-46c8-94b8-2a1a0560c3b9' 
+    }
+
+    const productosNuevos2 = {
+            title: 'pipa peterson',
+            price: '15000',
+            thumbnail: 'https://firebasestorage.googleapis.com/v0/b/mefe-pipas.appspot.com/o/pipa-pe01.jpg?alt=media&token=3a1addb3-192a-46c8-94b8-2a1a0560c3b9' 
+    }
+
+    const productosNuevos3 = {
+        title: 'pipa mefe',
+        price: '20000',
+        thumbnail: 'https://firebasestorage.googleapis.com/v0/b/mefe-pipas.appspot.com/o/pipa-pe01.jpg?alt=media&token=3a1addb3-192a-46c8-94b8-2a1a0560c3b9' 
+    }
+
+    const todosLosProductos = await productos.getAll();
+
+    await productos.save(productosNuevos1);
+    await productos.save(productosNuevos2);
+    await productos.save(productosNuevos3);
+
+    return todosLosProductos;
+    
+}
+    
+module.exports = live();
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ desafioClase6/server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const live = require('./contenedor');
+const app = express();
+
+const server = app.listen(8080, () => {
+    console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
+});
+
+server.on('error', error => console.log(`Error en servidor ${error}`));
+
+const promise = async () => {
+
+    const productos = await live; 
+
+    const aleatorio = () => {
+        let random = productos[Math.floor(Math.random() * productos.length)];
+        return random
+    }
+    
+    app.get('/', (req, res) => {
+        res.send('<h1 style="color:blue;">Desafio Express! acceda a /productos y /productoRandom</h1>')
+    });
+
+    app.get('/productos', (req, res) => {
+        res.send(`${JSON.stringify(productos, null, 2)}`)
+    });
+
+    app.get('/productoRandom', (req, res) => {
+        res.send(`${JSON.stringify(aleatorio())}`)
+    });
+    
+}
+
+promise();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// productos.forEach(element => {
+//     `
+//         <h3>${element.title}</h3>
+//         <h4>${element.price}</h4>
+//         <h4>${element.id}</h4>
+//         <img src="${element.thumbnail}"/>`   
+// })
