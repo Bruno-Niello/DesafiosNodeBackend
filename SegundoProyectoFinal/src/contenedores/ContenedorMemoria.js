b87c6b2d1ad2d319b31c5726f1164b4735ee5bb9
Bruno Niello <brunoniello@gmail.com>
2022-09-24 01:36:38 -0300

    segunda entrega

--- SegundoProyectoFinal/src/contenedores/ContenedorMemoria.js
+++ SegundoProyectoFinal/src/contenedores/ContenedorMemoria.js
@@ -1,32 +1,61 @@
 class ContenedorMemoria {
-
     constructor() {
-        this.elementos = []
+      this.productos = [];
+      this.id = 1;
     }
-
+  
     listar(id) {
-        
+      const products = this.listarAll();
+      const lastElement = products[products.length - 1];
+      const productById = products.find((p) => p.id == id);
+      if (lastElement >= productById) {
+        return productById;
+      } else {
+        throw new Error();
+      }
     }
-
+  
     listarAll() {
-        
+      return this.productos;
     }
-
-    guardar(elem) {
-
+  
+    guardar(prod) {
+      try {
+        this.productos.push({
+          ...prod,
+          date: new Date().toLocaleString(),
+          id: this.id++,
+        });
+      } catch (error) {
+        console.error(error);
+        return [];
+      }
     }
-
-    actualizar(elem) {
- 
+  
+    actualizar(prod, id) {
+      const products = this.listarAll();
+      let lastElement = products[products.length - 1];
+      let productById = products.find((p) => p.id == id);
+      if (lastElement >= productById) {
+        productById.title = prod.title;
+        productById.price = prod.price;
+        productById.thumbnail = prod.thumbnail;
+      } else {
+        throw new Error();
+      }
     }
-
+  
     borrar(id) {
-
+      const products = this.listarAll();
+      const lastElement = products[products.length - 1];
+      let productById = products.find((p) => p.id == id);
+      if (lastElement >= productById) {
+        const position = products.indexOf(productById);
+        products.splice(position, 1);
+      } else {
+        throw new Error();
+      }
     }
-
-    borrarAll() {
-    
-    }
-}
-
-export default ContenedorMemoria
+  }
+  
+  export default ContenedorMemoria;
